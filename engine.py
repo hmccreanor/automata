568d5209313696f2f43961414f25f6a1a84d6cd0
McCreanor (P-L6) Hamish <15McCreanorH@harrowschool.org.uk>
2019-08-25 18:12:48 +0100

    Update engine.py
    Added translation and canvas views, still kind of buggy though

--- engine.py
+++ engine.py
@@ -3,20 +3,41 @@ Pretty basic automata engine
 Takes in a 2d matrix of 1s and 0s
 Should be able to draw matrix to screen
 Moving/scaling should also be implemented
+
+The way I plan on implementing scaling is for there to be the concept of a canvas
+Only a slice of the canvas is displayed at any given time.
+Scaling/moving across the image will just correspond to changing the parameters of our slicing function
 '''
 
 import cv2
 import numpy as np
 
-size = [640, 480]
+window_size = (640, 480)
+canvas_size = [640, 480]
+
+def canvas_slice(c, top_left_coords, slice_dims):
+    return c[top_left_coords[1] : top_left_coords[1] + slice_dims[1], top_left_coords[0] : top_left_coords[0] + slice_dims[0]]
+
+frame_coord = np.array([0, 0])
+frame_dims = np.array([100, 100])
+
+cells = np.random.randint(2, size = canvas_size).astype(float)
 
 while True:
-    cells = np.random.randint(2, size = size).astype(float)
 
-    cv2.imshow("Cells", cells)
+    cv2.imshow("Cells", cv2.resize(canvas_slice(cells, frame_coord, frame_dims), window_size, interpolation = cv2.INTER_NEAREST))
 
-    if cv2.waitKey(50) & 0xFF == ord("q"):
+    key = chr(cv2.waitKey(1000) & 0xFF)
+
+    if key == "q":
         break
-
+    elif key == "a":
+        frame_coord[0] -= 1
+    elif key == "d":
+        frame_coord[0] += 1
+    elif key == "s":
+        frame_coord[1] += 1
+    elif key == "w":
+        frame_coord[1] -= 1
 
 cv2.destroyAllWindows()
