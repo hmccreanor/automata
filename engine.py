e0bee873758037dfa2175a85ecef0b015fa7226e
McCreanor (P-L6) Hamish <15McCreanorH@harrowschool.org.uk>
2019-08-25 17:39:24 +0100

    Update engine.py
    Realised using opencv is probably smarter for this

--- engine.py
+++ engine.py
@@ -5,32 +5,18 @@ Should be able to draw matrix to screen
 Moving/scaling should also be implemented
 '''
 
+import cv2
 import numpy as np
-import pygame
-
-pygame.init()
 
 size = [640, 480]
 
-screen = pygame.display.set_mode(size)
+while True:
+    cells = np.random.randint(2, size = size).astype(float)
 
-test_matrix = np.random.randint(2, size = size[::-1])
-print(test_matrix)
+    cv2.imshow("Cells", cells)
 
-playing = True
+    if cv2.waitKey(50) & 0xFF == ord("q"):
+        break
 
-while playing:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            playing = False
 
-    screen.fill((255, 255, 255))
-
-    for y in range(test_matrix.shape[0]):
-        for x in range(test_matrix.shape[1]):
-            if test_matrix[y][x] == 0:
-                pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(x, y, 1, 1))
-            else:
-                pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(x, y, 1, 1))
-
-    pygame.display.flip()
+cv2.destroyAllWindows()
